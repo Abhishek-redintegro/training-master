2d5d22a04cfb59cdaaaf329b6595d5c5847885d0
unknown <abhishekbhansali07@gmail.com>
2020-11-05 14:37:21 +0530

    Five Nov Commit

--- blog/schema/person_schema.py
+++ blog/schema/person_schema.py
@@ -2,6 +2,64 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from blog.models import Person as PersonModel
 
+
 class Person(DjangoObjectType):
     class Meta:
         model = PersonModel
+
+
+class CreatePerson(graphene.Mutation):
+    class Arguments:
+        u_name = graphene.String(required=True)
+        u_desc = graphene.String(required=True)
+        u_phone_no = graphene.String(required=True)
+        u_email = graphene.String(required=True)
+        u_password = graphene.String(required=True)
+
+    person = graphene.Field(Person)
+
+    def mutate(self, info, u_name, u_desc, u_phone_no, u_email, u_password):
+        new_person = PersonModel(
+            u_name=u_name,
+            u_desc=u_desc,
+            u_phone_no=u_phone_no,
+            u_email=u_email,
+            u_password=u_password
+        )
+        new_person.save()
+        return CreatePerson(person=new_person)
+
+class UpdatePerson(graphene.Mutation):
+    class Arguments:
+        u_email = graphene.String(required=True)
+        id = graphene.Int()
+    
+    message = graphene.String()
+
+    def mutate(self,info,u_email,id):
+        old_person = PersonModel.objects.get(pk=id)
+        old_email = old_person.u_email
+        old_person.u_email = u_email
+        old_person.save()
+        return UpdatePerson(message=f'Old Email:{old_email} New Email:{u_email} For Id:{id}')
+
+
+class DeletePerson(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int()
+
+    message = graphene.String()
+
+    def mutate(self,info,id):
+        old_person = PersonModel.objects.get(pk=id)
+        old_person.delete()
+        return DeletePerson(message=f'Deleted Data For Id:{id}')
+# mutation{
+#   createPerson(uDesc:"Regular", uEmail:"Abhisshek@gmail.com",uName:"Abhishek",
+#     uPassword :"ABSA1212", uPhoneNo: "7218983902")
+#   {
+#     person{
+#       id
+#     }
+#   }
+# }
--- blog/schema/schema.py
+++ blog/schema/schema.py
@@ -1,18 +1,21 @@
 import graphene
 from blog.schema.blog_schema import Blog, BlogModel
-from blog.schema.person_schema import Person,PersonModel
+from blog.schema.person_schema import Person, PersonModel
+from blog.schema.blog_schema import Blog, BlogModel, CreateBlog, UpdateBlog, DeleteBlog
+from blog.schema.person_schema import Person, PersonModel, CreatePerson, UpdatePerson, DeletePerson
+
 
 class Query(graphene.ObjectType):
     # Blog Models Start----------------
     allBlogs = graphene.List(Blog)
-    blog = graphene.Field(Blog,id=graphene.Int(required=True))
+    blog = graphene.Field(Blog, id=graphene.Int(required=True))
     # Blog Models End----------------
 
     # Person Model Start------------------
     allPerson = graphene.List(Person)
-    person = graphene.Field(Person,id=graphene.Int(required=True))
+    person = graphene.Field(Person, id=graphene.Int(required=True))
     # Person Model End-----------------
-    
+
     # Blog
     def resolve_allBlogs(self, info):
         return BlogModel.objects.all()
@@ -21,8 +24,17 @@ class Query(graphene.ObjectType):
         return BlogModel.objects.get(pk=id)
 
     # Person
-    def resolve_allPerson(self,info):
+    def resolve_allPerson(self, info):
         return PersonModel.objects.all()
 
-    def resolve_person(self,info,id):
+    def resolve_person(self, info, id):
         return PersonModel.objects.get(pk=id)
+
+
+class Mutations(graphene.ObjectType):
+    create_blog = CreateBlog.Field()
+    create_person = CreatePerson.Field()
+    update_blog = UpdateBlog.Field()
+    update_person = UpdatePerson.Field()
+    delete_blog = DeleteBlog.Field()
+    delete_person = DeletePerson.Field()
